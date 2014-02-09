927178e683d0d25aa67334568cfceaf33d938424
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-02-10 00:35:20 +0200

    started working on backend

---
+++ carti.java
@@ -0,0 +1,40 @@
+import java.util.*
+
+class Carte
+{
+	int nArrack, nHealth;
+
+}
+
+class Justitiar extends Carte
+{
+	boolean bIsMilitian;
+	boolean bIsMilitar;
+
+}
+
+class JucatorCamp extends Carte
+{
+	boolean bIsSoarece;
+	boolean bIsCocalar;
+	boolean bIsMafiot;
+	boolean bIsDiva;
+	boolean bIsPitipoanca;
+	boolean bIsBaba;
+
+}
+
+class Moloz extends Carte
+{
+
+}
+
+class Deck 
+{
+	List<Carte> carti = new List<Carte>();
+}
+
+class Player
+{
+
+}
