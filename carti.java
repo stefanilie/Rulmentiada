337b25f6576a54498d9ab7e91f724257f4921a61
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-02-11 01:18:58 +0200

    work

--- carti.java
+++ carti.java
@@ -2,7 +2,87 @@ import java.util.*
 
 class Carte
 {
-	int nArrack, nHealth;
+	private int _nAttack;
+	private int _nHealth;
+	private int _nMana;
+
+	public getAttack()
+	{
+		return this._nAttack;
+	}
+
+	public getHealth()
+	{
+		return this._nHealth;
+	}
+
+	public getMana()
+	{
+		return this._nMana;
+	}
+
+	public setAttack(int nValue)
+	{
+		this._nAttack = nValue;
+	}
+
+	public setHealth(int nValue)
+	{
+		this._nHealth = nValue;
+	}
+
+	public setMana(int nValue)
+	{
+		this._nMana = nValue;
+	}
+
+	public Carte()
+	{
+		Random rand = new Random()
+		int max = 1;
+		int min = 0;
+		float fitness = rand.nextFloat() * (max - min) + min;
+		if (fitness>0 && fitness<=0.25)
+		{
+			this._nHealth = 0 + (int)(Math.random() * ((3 - 0) + 1));
+		}
+		else if(fitness>0.25 && fitness<=0.5)
+		{
+			this._nHealth = 3 + (int)(Math.random() * ((5 - 3) + 1));
+		}
+		else if(fitness>0.5 && fitness<=0.7)
+		{
+			this._nHealth = 5 + (int)(Math.random() * ((7 - 5) + 1));
+		}
+		else if(fitness>0.7 && fitness<=1)
+		{
+			this._nHealth = 7 + (int)(Math.random() * ((10 - 7) + 1));
+		}
+
+		float power = rand.nextFloat() * (max -min) + min;
+		if (power>0 && power<=0.25)
+		{
+			this._nHealth = 0 + (int)(Math.random() * ((3 - 0) + 1));
+		}
+		else if(power>0.25 && power<=0.5)
+		{
+			this._nHealth = 3 + (int)(Math.random() * ((5 - 3) + 1));
+		}
+		else if(power>0.5 && power<=0.7)
+		{
+			this._nHealth = 5 + (int)(Math.random() * ((7 - 5) + 1));
+		}
+		else if(power>0.7 && power<=1)
+		{
+			this._nHealth = 7 + (int)(Math.random() * ((10 - 7) + 1));
+		}
+
+		if(this._nHealth + this._nAttack < 10)
+		{
+			
+		}
+
+	}
 
 }
 
@@ -31,7 +111,26 @@ class Moloz extends Carte
 
 class Deck 
 {
-	List<Carte> carti = new List<Carte>();
+	List<Carte> carti;
+
+	public Deck()
+	{
+		carti = new List<Carte>();
+	}
+
+	public populateDeck()
+	{
+		/*
+		 * algortim semi-inteligent care sa imi adauge in pachet 
+		 * in jur de 50 de carti.
+		 */
+		int nDeckLimit = 50;
+
+		for(int i=0; i<nDeckLimit; i++)
+		{
+
+		}
+	}
 }
 
 class Player
